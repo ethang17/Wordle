550806c65b98979fb0a4af38087fcac9ee49033d
Ethan Grim <ethangrimmy33@gmail.com>
2023-11-09 19:36:26 -0500

    Accidental Y and G

--- input.py
+++ input.py
@@ -17,8 +17,12 @@ def inCombined(word):
 def score(guess, answer):
     resultsG = scoreG(guess, answer)
     print(resultsG)
-    resultsAll = scoreY(guess, resultsG)
+    print("G^")
+    resultsY = scoreY(guess, answer)
+    resultsYNoMult = removeMultiples(answer, resultsY)
+    resultsAll = merge(resultsG, resultsYNoMult, guess)
     return resultsAll
+
 def scoreG(guess, answer):
     resultsG=["","","","",""]
     for x in range(5):
@@ -27,14 +31,45 @@ def scoreG(guess, answer):
         else:
             resultsG[x] = guess[x]
     return resultsG
-def scoreY(guess,resultsG):
-    resultsAll=["","","","",""]
+
+def scoreY(guess,answer):
+    resultsY=["","","","",""]
     for x in range(5):
-        if guess[x] == resultsG[0] or guess[x]==resultsG[1]or guess[x]==resultsG[2]or guess[x]==resultsG[3]or guess[x]==resultsG[4]:
-            resultsAll[x] = guess[x] +"(Y)"
+        if guess[x] == answer[0] or guess[x]==answer[1]or guess[x]==answer[2]or guess[x]==answer[3]or guess[x]==answer[4]:
+            resultsY[x] = guess[x] +"(Y)"
         else:
-            resultsAll[x] = resultsG[x]
-    return resultsAll
+            resultsY[x] = guess[x]
+    return resultsY
+
+def removeMultiples(answer, resultsY):
+    for i in range(5):
+        currentLetter = answer[i]
+        currentLetterCount = 0
+        for j in range(5):
+            if answer[j] == currentLetter:
+                currentLetterCount+=1
+                print(currentLetter +": "+str(currentLetterCount))
+
+        currentLetterYellows = 0
+        for k in range(5):
+            if resultsY[k] == (currentLetter + "(Y)"):
+                currentLetterYellows+=1
+                print(currentLetter + "(Y): "+str(currentLetterYellows))
+                if currentLetterYellows > currentLetterCount:
+                    resultsY[k] = currentLetter
+        print(resultsY)
+    return resultsY
+
+def merge(g, y, guess):
+    results = ["","","","",""]
+    for i in range(5):
+        if "(G)" in g[i]:
+            results[i] = g[i]
+        elif "(Y)" in y[i]:
+            results[i] = y[i]
+        else:
+            results[i] = guess[i]
+    return results
 
 
 
